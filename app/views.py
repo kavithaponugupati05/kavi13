1172e9765ec6ed6882b78ede91641d22a4ea3386
manu678 <kavithaponugupati05@gmail.com>
2021-08-13 14:24:52 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def kavitha(request):
+    return render(request,'manu.html')
+
+def nag(request):
+    return render(request,'lucky.html')
+    
